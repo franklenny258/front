a4d0e16f586d5110b20a54bcecccc97050af2486
Frank Ramirez <franklenny258@gmail.com>
2024-04-25 07:45:37 -0400

    [enhancement] go mobile with capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'front.mobile.app',
+  appName: 'front-mobile',
+  webDir: 'dist'
+};
+
+export default config;
--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,8 +1,13 @@
 import { useRouteError } from 'react-router-dom';
 import css from './index.module.css';
 
+interface RouteError {
+  statusText?: string;
+  message?: string;
+}
+
 export const ErrorPage = () => {
-  const error = useRouteError();
+  const error = useRouteError() as RouteError;
 
   return (
     <main className={css.main}>
